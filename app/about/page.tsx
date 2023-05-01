8c247403e6c54f69a412e619e3367b8e3a1a8786
alanagoyal <alanaan@gmail.com>
2023-04-30 18:57:28 -0700

    fix apos

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,19 +13,11 @@ const About = () => {
       <h1 className="text-4xl font-bold mb-4">About</h1>
       <div className="text-xl font-base mb-6 max-w-lg text-center">
         <p>
-          The YC SAFE (Simple Agreement for Future Equity) is a widely used
-          investment instrument created by Y Combinator, a startup accelerator
-          and venture capital firm. It allows startups to raise funds from
-          investors without setting a valuation for the company.
+          Y Combinator introduced the safe (simple agreement for future equity)
+          in late 2013, and since then, it has been used by almost all YC
+          startups and countless non-YC startups as the main instrument for
+          early-stage fundraising.
         </p>
-        <p className="mt-4">
-          Instead, the investor receives the right to receive equity in the
-          company at a future date, usually at the next round of funding or an
-          acquisition. This allows the company to raise funds without the need
-          for a complex negotiation of the company's worth.
-        </p>
-      </div>
-      <div className="text-xl font-base mb-6 max-w-lg text-center">
         <p>There are two types of YC SAFE notes: Discount and Valuation Cap.</p>
         <p className="mt-4">
           A Discount SAFE gives investors the right to purchase equity at a
@@ -35,10 +27,10 @@ const About = () => {
         </p>
         <p className="mt-4">
           A Valuation Cap SAFE sets a maximum valuation at which the investor
-          can convert their investment into equity. If the company's valuation
-          at the next round of funding is higher than the cap, the investor's
-          investment will be converted into equity at the lower cap valuation,
-          giving them a larger share of the company.
+          can convert their investment into equity. If the company&apos;s
+          valuation at the next round of funding is higher than the cap, the
+          investor&apos;s investment will be converted into equity at the lower
+          cap valuation, giving them a larger share of the company.
         </p>
       </div>
       <Link href="https://www.ycombinator.com/documents">

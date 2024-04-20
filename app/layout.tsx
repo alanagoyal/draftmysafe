993a10764096ee038dfaa8eb51aaf020ce563c81
alanagoyal <alanaan@gmail.com>
2024-04-19 17:37:48 -0700

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,70 +1,63 @@
-import "@/styles/globals.css"
-import { Metadata } from "next"
+import type { Metadata } from "next"
 
-import { siteConfig } from "@/config/site"
-import { fontSans } from "@/lib/fonts"
+import "@/styles/globals.css"
+import { Inter } from "next/font/google"
+
 import { cn } from "@/lib/utils"
+import { Toaster } from "@/components/ui/toaster"
 import { SiteHeader } from "@/components/site-header"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+})
+
 export const metadata: Metadata = {
-  title: {
-    default: "SafeBase | Skip the paperwork",
-    template: `%s - SafeBase`,
-  },
-  description: "Generate a SAFE",
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "@alanaagoyal",
-    creator: "@alanaagoyal",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: siteConfig.ogImage,
-  },
+  title: "Safely",
+  description: "Skip the paperwork & generate a YC SAFE in minutes",
 }
 
-interface RootLayoutProps {
+export default async function RootLayout({
+  children,
+}: {
   children: React.ReactNode
-}
-
-export default function RootLayout({ children }: RootLayoutProps) {
+}) {
   return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta property="og:title" content="Safely"></meta>
+        <meta
+          property="og:description"
+          content="Skip the paperwork & generate a YC SAFE in minutes"
+        ></meta>
+      </head>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
         >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
+          <div vaul-drawer-wrapper="">
+            <div className="relative flex flex-col bg-background">
               <SiteHeader />
-              <div className="flex-1">{children}</div>
+              <main className="flex-1">
+                <div className="flex flex-col items-center pt-10 py-2 max-w-5xl mx-auto">
+                  {children}
+                </div>
+              </main>
             </div>
-            <TailwindIndicator />
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
+          </div>
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
   )
 }

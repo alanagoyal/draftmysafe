8aaffbd22108702812e43fe0c68fa816b22d4762
alanagoyal <alanaan@gmail.com>
2024-05-15 19:26:10 -0700

    show investments for investor or founder in investment

--- app/investments/page.tsx
+++ app/investments/page.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link"
 import { redirect } from "next/navigation"
 import { createClient } from "@/utils/supabase/server"
-import Investments from "@/components/investments"
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
 import { Briefcase } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import Investments from "@/components/investments"
 
 export default async function InvestmentsPage() {
   const supabase = createClient()
@@ -15,20 +16,20 @@ export default async function InvestmentsPage() {
     redirect("/login")
   }
 
-  const { data: userData, error: userError} = await supabase
+  const { data: userData, error: userError } = await supabase
     .from("users")
     .select()
     .eq("auth_id", user?.id)
     .single()
 
-    if (userError) {
-        console.error(userError)
-    }
+  if (userError) {
+    console.error(userError)
+  }
 
-    const {data: investmentData, error: investmentError} = await supabase
+  const { data: investmentData, error: investmentError } = await supabase
     .from("investments")
     .select(
-        `
+      `
         id,
         purchase_amount,
         investment_type,
@@ -40,26 +41,25 @@ export default async function InvestmentsPage() {
         investor:users!investor_id (name, title, email),
         fund:funds (name, byline, street, city_state_zip)
       `
-      )
-    .eq("created_by", userData.auth_id)
-
-    if (investmentError) {
-        console.error(investmentError)
-    }
-
-  return (
-    (investmentData && investmentData.length > 0) ? (
-      <div className="flex w-full justify-center min-h-screen">
-        <Investments investments={investmentData} />
-      </div>
-    ) : (
-      <div className="w-full px-4 flex justify-center items-center flex-col min-h-screen">
-        <h1 className="text-2xl text-center font-bold mb-4">You haven&apos;t created <br /> any investments yet</h1>
-        <Link className="flex justify-center pt-2" href="/new">
-          <Button variant="outline">Get Started</Button>
-        </Link>
-      </div>
     )
+    .or(`investor_id.eq.${userData.id},founder_id.eq.${userData.id}`)
+
+  if (investmentError) {
+    console.error(investmentError)
+  }
+
+  return investmentData && investmentData.length > 0 ? (
+    <div className="flex w-full justify-center min-h-screen">
+      <Investments investments={investmentData} />
+    </div>
+  ) : (
+    <div className="w-full px-4 flex justify-center items-center flex-col min-h-screen">
+      <h1 className="text-2xl text-center font-bold mb-4">
+        You haven&apos;t created <br /> any investments yet
+      </h1>
+      <Link className="flex justify-center pt-2" href="/new">
+        <Button variant="outline">Get Started</Button>
+      </Link>
+    </div>
   )
 }
-

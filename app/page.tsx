4366fbef135bd0d8573328e8bb2bea241abf88a7
alanagoyal <alanaan@gmail.com>
2024-05-24 14:50:12 -0400

    update hero

--- app/page.tsx
+++ app/page.tsx
@@ -6,55 +6,51 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 export default async function Home() {
   const sections = [
     {
-      title: "Pre-Fill & Generate in Minutes",
+      title: "Signature Blocks",
       subtitle:
         "Save time with our streamlined YC SAFE creation process. Create your account, store your signature block, and effortlessly pre-fill future agreements. Focus more on your investments and less on paperwork.",
     },
     {
-      title: "Securely Request Founder Details",
+      title: "Secure Sharing",
       subtitle:
         "Send a secure link to founders to gather necessary information for the SAFE agreement. Our platform ensures data security and simplicity, making the process smooth for both you and the founders.",
     },
     {
-      title: "Secure E-Signatures Made Easy",
+      title: "Easy E-Signatures",
       subtitle:
         "Finalize your agreements with ease using our integrated e-signature feature. Send out documents for signing securely and keep everything organized within our app, ensuring a professional and efficient investment process.",
     },
   ]
 
   return (
-    <div className="w-full pt-10 min-h-screen">
-      <div className="mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex max-w-[980px] flex-col items-start gap-2">
-          <div className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-            Draftmysafe
-          </div>
-          <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl pt-2">
-            Skip the paperwork & generate a YC SAFE agreement in minutes
-          </p>
+    <div className="w-full pt-10 min-h-screen flex flex-col items-center">
+      <div className="text-center w-full pt-20 flex flex-col justify-center items-center">
+        <div className="text-5xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
+          Draftmysafe
         </div>
-        <div className="flex py-4">
+        <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl pt-4">
+          Skip the paperwork & generate a YC SAFE agreement in minutes
+        </p>
+        <div className="py-4">
           <Link href="/new">
-            <Button>Get Started</Button>
+            <Button className="w-48">Get Started</Button>
           </Link>
         </div>
-        <div className="flex flex-col md:flex-row justify-around items-stretch my-20">
-          {sections.map((section, index) => (
-            <Card
-              key={index}
-              className={`flex flex-col w-full md:w-1/3 shadow-lg rounded-lg ${
-                index === 1 ? "md:mx-4 my-2 md:my-0" : "my-4 md:my-0"
-              }`}
-            >
-              <CardHeader>
-                <CardTitle>{section.title}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>{section.subtitle}</p>
-              </CardContent>
-            </Card>
-          ))}
-        </div>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center items-stretch pt-10 w-full">
+        {sections.map((section, index) => (
+          <Card
+            key={index}
+            className={`flex flex-col w-full md:w-1/3 shadow-lg rounded-lg mx-2 my-4`}
+          >
+            <CardHeader>
+              <CardTitle>{section.title}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>{section.subtitle}</p>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </div>
   )

b50ecb1cd48296726f6f377f50354aa64a14e4ef
alanagoyal <alanaan@gmail.com>
2024-05-24 14:02:01 -0400

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -22,78 +22,6 @@ export default async function Home() {
     },
   ]
 
-  const customerQuotes = [
-    {
-      avatar: "guillermo.jpeg",
-      name: "Guillermo Rauch",
-      companyUrl: "https://vercel.com",
-      companyName: "Vercel",
-      role: "Founder",
-      quote: (
-        <div className="flex flex-row items-stretch">
-          <div className="flex w-full sm:w-2/3 items-center justify-center">
-            &quot;I used @alanaagoyal&apos;s AI-based brand generation tool and
-            it&apos;s shockingly good.
-            <br />
-            <br />
-            It coined &apos;brainy&apos; and generated this cool logo in a
-            couple seconds 😁. Quite the head-start to get your idea into the
-            world.&quot;
-          </div>
-          <div
-            className="flex sm:w-1/3 items-center justify-center mx-4 hidden sm:flex"
-            style={{ marginTop: "-20px" }}
-          >
-            <img
-              src="brainy.png"
-              alt="Brainy"
-              className="object-cover w-40 h-40 rounded-md"
-            />
-          </div>
-        </div>
-      ),
-    },
-    {
-      avatar: "paul.jpeg",
-      name: "Paul Dornier",
-      companyUrl: "https://alpharun.com",
-      companyName: "Alpharun",
-      role: "Co-Founder",
-      quote: (
-        <>
-          &quot;When we started our company we knew we needed a simple,
-          memorable name that built trust with our customers. Before Branded, we
-          spent hours coming up with names and looking up domain availability.
-          With Branded we found alpharun.com with a click, and haven&apos;t
-          looked back since.
-          <br />
-          <br />
-          The process was seamless, and the results were beyond my expectations.
-          Highly recommend!&quot;
-        </>
-      ),
-    },
-    {
-      avatar: "jordan.jpeg",
-      name: "Jordan Singer",
-      companyUrl: "https://figma.com",
-      companyName: "Figma",
-      role: "Product Designer",
-      quote: (
-        <>
-          &quot;I love how Alana builds tools that not only her portfolio
-          companies but anyone can use. Branded is a great example of a simple
-          idea to help come up with one of the most critical parts of your
-          company: its name.
-          <br />
-          <br />
-          Namestorming with AI gives you lots of interesting ideas, and you can
-          make a logo and get your domain too!&quot;
-        </>
-      ),
-    },
-  ]
-
   return (
     <div className="w-full pt-10 min-h-screen">
       <div className="mx-auto px-4 sm:px-6 lg:px-8">
@@ -127,43 +55,6 @@ export default async function Home() {
             </Card>
           ))}
         </div>
-        {/*       <div className="my-20 flex justify-center w-full">
-          <Carousel className="w-4/5 justify-center">
-            <CarouselContent>
-              {customerQuotes.map((customerQuote, index) => (
-                <CarouselItem key={index}>
-                  <Card className="p-4 shadow-lg rounded-lg">
-                    <CardContent>
-                      <a
-                        href={customerQuote.companyUrl}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="w-full flex items-center"
-                      >
-                        <img
-                          src={customerQuote.avatar}
-                          alt="Customer avatar"
-                          className="w-16 h-16 rounded-full mr-4"
-                        />
-                        <div className="flex flex-col">
-                          <h3 className="text-xl font-bold">
-                            {customerQuote.name}
-                          </h3>
-                          <p className="text-sm">
-                            {customerQuote.role}, {customerQuote.companyName}
-                          </p>
-                        </div>
-                      </a>
-                      <div className="mt-4 text-sm">{customerQuote.quote}</div>
-                    </CardContent>
-                  </Card>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        </div> */}
       </div>
     </div>
   )

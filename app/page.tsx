255358571c2550789e038e29c11a704b237d92b2
alanagoyal <alanaan@gmail.com>
2024-05-25 10:36:18 -0400

    add icons and curve button

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,27 @@ import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Icons } from "@/components/icons"
 
 export default async function Home() {
   const sections = [
     {
-      title: "Signature Blocks",
+      title: "Document Creation",
       subtitle:
         "Save time with our streamlined YC SAFE creation process. Create your account, store your signature block, and effortlessly pre-fill future agreements. Focus more on your investments and less on paperwork.",
+      icon: Icons.files,
     },
     {
       title: "Secure Sharing",
       subtitle:
         "Send a secure link to founders to gather necessary information for the SAFE agreement. Our platform ensures data security and simplicity, making the process smooth for both you and the founders.",
+      icon: Icons.lock,
     },
     {
       title: "Easy E-Signatures",
       subtitle:
         "Finalize your agreements with ease using our integrated e-signature feature. Send out documents for signing securely and keep everything organized within our app, ensuring a professional and efficient investment process.",
+      icon: Icons.signature,
     },
   ]
 
@@ -33,21 +37,24 @@ export default async function Home() {
         </p>
         <div className="py-4">
           <Link href="/new">
-            <Button className="w-48">Get Started</Button>
+            <Button className="w-48 h-12 rounded-full">Get Started</Button>
           </Link>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row justify-center items-stretch pt-10 w-full">
+      <div className="flex flex-col md:flex-row justify-center items-stretch pt-8 w-full">
         {sections.map((section, index) => (
           <Card
             key={index}
             className={`flex flex-col w-full md:w-1/3 shadow-lg rounded-lg mx-2 my-4`}
           >
             <CardHeader>
-              <CardTitle>{section.title}</CardTitle>
+              <div className="flex flex-col items-center">
+                <section.icon className="w-12 h-12 my-2" />
+                <CardTitle>{section.title}</CardTitle>
+              </div>
             </CardHeader>
             <CardContent>
-              <p>{section.subtitle}</p>
+              <p className="text-sm text-center">{section.subtitle}</p>
             </CardContent>
           </Card>
         ))}

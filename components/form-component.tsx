ba1219b6ae59451b1af04cb02f47a5bb14a657fb
alanagoyal <alanaan@gmail.com>
2024-05-31 13:45:28 -0700

    update toast

--- components/form-component.tsx
+++ components/form-component.tsx
@@ -767,7 +767,7 @@ export default function FormComponent({ userData }: { userData: any }) {
       toast({
         title: "Congratulations!",
         description:
-          "Your information has been saved. You'll receive an email with the next steps shortly.",
+          "Your information has been saved. You'll receive an email with the next steps once all parties have provided their information.",
       })
     }
   }

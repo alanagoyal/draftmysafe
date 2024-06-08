3a3bb732883b7697c7bd397826805c1f857f66f6
Alana Goyal <alanaan@gmail.com>
2024-06-08 14:03:06 -0700

    make investment email editable (#18)
    * make investment email editable
    
    * remove line breaks in email htl
    
    * update to use html parser
    
    * remove imports
    
    * update summary
    
    * fix summary text
    
    * fix loading save vs next

--- components/templates/email-investment.tsx
+++ components/templates/email-investment.tsx
@@ -1,24 +1,10 @@
-import * as React from "react"
+import * as React from "react";
+import parse from "html-react-parser";
 
 interface EmailTemplateProps {
-  investmentData: any
+  emailContent: string;
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
-  investmentData,
-}) => (
-  <div>
-    <p>Hi {investmentData.founder.name.split(" ")[0]},</p>
-    <p>
-      {investmentData.fund.name} has shared a SAFE agreement with you. Please
-      find the document attached to this email and find a brief summary of the
-      document and its terms below.
-    </p>
-    <p>{investmentData.summary}</p>
-    <p>
-      Disclaimer: This summary is for informational purposes only and does not
-      constitute legal advice. For any legal matters or specific questions, you
-      should consult with a qualified attorney.
-    </p>
-  </div>
-)
+  emailContent,
+}) => <div>{parse(emailContent)}</div>;
